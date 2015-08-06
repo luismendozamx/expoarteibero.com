7c5d414277d9c209b6d73396e5567ab82b6fbb73
Luis Mendoza <luis@mendoza.io>
2015-08-06 11:45:33 -0500

    Make password recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable, :recoverable
 
   validates :first_name, :last_name, :email, :read_agreement, presence: true
 

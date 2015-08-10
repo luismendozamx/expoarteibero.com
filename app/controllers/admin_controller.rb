04f3af809ddf860c9fda56b71be4b1ff8696b4ad
Luis Mendoza <luis@mendoza.io>
2015-08-09 21:24:31 -0500

    Updated views and implement non admin redirect

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  before_action :admin_user, only: [:show, :index]
 
   def index
     @user_count = User.count
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def admin_user
+    unless user_signed_in? && current_user.is_admin?
+      flash[:notice] = "You are not authorized to see this page."
+      redirect_to root_path # halts request cycle
+    end
+  end
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :first_name, :last_name, :read_agreement, :password, :password_confirm) }
